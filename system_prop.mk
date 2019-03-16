ae7a06413c16003224abb11532437d2f6338e03e
Hazari PraveenKumar <phazari@codeaurora.org>
2019-03-16 15:51:19 +0100

    RMX2151: Disable VSync for CPU rendered apps
    Choreographer VSync has some performance impact for CPU rendered apps
    as the app has to wait for VSync to start a new frame draw. When the
    frame draw time exceeds one VSync, this has a direct impact on the FPS.
    debug.cpurend.vsync property controls the VSync for CPU rendered apps.
    
    Disable VSync for CPU rendered app, by setting this property to false.

--- system_prop.mk
+++ system_prop.mk
@@ -37,6 +37,7 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     debug.sf.disable_hwc_vds=1 \
     debug.sf.latch_unsignaled=1 \
+    debug.cpurend.vsync=false \
     ro.surface_flinger.has_HDR_display=true \
     ro.surface_flinger.has_wide_color_display=true \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
