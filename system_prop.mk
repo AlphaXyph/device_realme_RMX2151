80146f3af2f8351918967cfd235b162d3dd20128
zjunior06 <zjunior06@pixelexperience.org>
2021-04-15 22:19:56 +0530

    RMX2151: Add some props for better ram management

--- system_prop.mk
+++ system_prop.mk
@@ -54,3 +54,15 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.lmk.psi_complete_stall_ms=150 \
     ro.lmk.swap_free_low_percentage=20 \
     ro.lmk.kill_timeout_ms=100
+	
+# Tweak the memory management of the device, enable more background apps.. etc..
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.fha_enable=true \
+    ro.sys.fw.bg_apps_limit=32 \
+    ro.config.dha_cached_max=16 \
+    ro.config.dha_empty_max=42 \
+    ro.config.dha_empty_init=32 \
+    ro.config.dha_lmk_scale=0.545 \
+    ro.config.dha_th_rate=2.3 \
+    ro.config.sdha_apps_bg_max=64 \
+    ro.config.sdha_apps_bg_min=8
