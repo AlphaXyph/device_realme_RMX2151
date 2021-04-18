7d7071bf4a7599f9fdc28c1c5a2005529f77c53d
Sarthak Roy <sarthakroy2002@gmail.com>
2021-04-18 10:55:32 +0530

    RMX2151: Fix a derp

--- aosp_RMX2151.mk
+++ aosp_RMX2151.mk
@@ -22,7 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/realme/RMX2151/device.mk)
 
 # Inherit some common PixelPlusUI stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
