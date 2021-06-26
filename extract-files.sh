7e85e2a9f09c044b870f65a80c80962857b1a957
AlphaXyph <55902316+AlphaXyph@users.noreply.github.com>
2021-06-26 14:06:36 +0530

    Making things bit more aosp friendly and cleaning ppui bringup

--- extract-files.sh
+++ extract-files.sh
@@ -25,9 +25,9 @@ export VENDOR=realme
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-ROOT="${MY_DIR}"/../../..
+ANDROID_ROOT="${MY_DIR}"/../../..
 
-HELPER="${ROOT}/vendor/aosp/build/tools/extract_utils.sh"
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
@@ -64,7 +64,7 @@ if [ -z "${SRC}" ]; then
 fi
 
 # Initialize the helper for device
-setup_vendor "${DEVICE}" "${VENDOR}" "${ROOT}" true "${CLEAN_VENDOR}"
+setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" true "${CLEAN_VENDOR}"
 
 extract "${MY_DIR}/proprietary-files.txt" "${SRC}" \
         "${KANG}" --section "${SECTION}"
