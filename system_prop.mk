742eeb1bcf66731264cd2b4fe7d56fc3eaba5540
Lucas Dupin <dupin@google.com>
2021-06-26 15:47:34 +0530

    RMX2151: Disable blur by default

--- system_prop.mk
+++ system_prop.mk
@@ -47,7 +47,8 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 # Blur
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.surface_flinger.supports_background_blur=1 \
-    ro.sf.blurs_are_expensive=1
+    ro.sf.blurs_are_expensive=1 \
+    persist.sys.sf.disable_blurs=1
 
 # LMKD
 PRODUCT_PRODUCT_PROPERTIES += \
