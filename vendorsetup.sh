fd6ae5124bdbfa44c77263ed6045107419320529
AlphaXyph <AlphaXyphoid@gmail.com>
2021-10-18 16:20:51 +0000

    RMX2151: Use A12 RealmeParts by sarthak

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 rm -rf packages/apps/RealmeParts
-git clone https://gitlab.com/alphaxyphoid/realmeparts.git packages/apps/RealmeParts
+git clone https://github.com/Realme-G70-Series/android_packages_apps_RealmeParts.git -b aosp12 packages/apps/RealmeParts
 
 rm -rf packages/apps/RealmeDirac
 git clone https://github.com/techyminati/android_packages_apps_RealmeDirac packages/apps/RealmeDirac
