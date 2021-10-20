c0a4fd6c9d138927f39fa01dafe139655c79dd20
AlphaXyph <AlphaXyphoid@gmail.com>
2021-10-21 00:10:05 +0530

    RMX2151: Just some mtk stuffs

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,3 +3,10 @@ git clone https://github.com/Realme-G70-Series/android_packages_apps_RealmeParts
 
 rm -rf packages/apps/RealmeDirac
 #git clone https://github.com/techyminati/android_packages_apps_RealmeDirac packages/apps/RealmeDirac
+
+#Cherry-pick some stuffs for A12
+cd system/vold && git fetch https://github.com/ArrowOS/android_system_vold/ && cherry-pick 7a18da8f8a657c3bda9732bb3101aa252ac4a08a && cd ../..
+cd external/selinux && git fetch https://github.com/ArrowOS/android_external_selinux/ && git cherry-pick I3d19545b4e50dc88179f5a7b2a30f827fe2955d2 && cd ../..
+cd framework/base && git fetch https://github.com/PixelBlaster-Temp/frameworks_base/ && git cherry-pick 512d8394a00f09f531da70ce274dbdb1d71cd718 & cd ../..
+
+
